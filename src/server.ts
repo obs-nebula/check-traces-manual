ebf1a35952fa36bd389f7d8f2370411281cdcf32
Helio Frota <00hf11@gmail.com>
2023-05-03 10:13:25 -0300

    feat: example with metrics

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,18 @@ import opentelemetry from '@opentelemetry/api';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+
 import {
   BasicTracerProvider,
   ConsoleSpanExporter,
   SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
+
+import {
+  MeterProvider,
+  PeriodicExportingMetricReader,
+} from '@opentelemetry/sdk-metrics';
 
 import { error, log } from 'console';
 
@@ -14,10 +21,12 @@ import express, { Express, Request, Response } from 'express';
 
 const exporter = new OTLPTraceExporter();
 
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: process.env.npm_package_name,
+});
+
 const provider = new BasicTracerProvider({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: process.env.npm_package_name,
-  }),
+  resource: resource,
 });
 
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
@@ -30,12 +39,31 @@ const tracer = opentelemetry.trace.getTracer(
   process.env.npm_package_version
 );
 
+const metricsExporter = new OTLPMetricExporter();
+const meterProvider = new MeterProvider({ resource: resource });
+meterProvider.addMetricReader(
+  new PeriodicExportingMetricReader({ exporter: metricsExporter })
+);
+
+const meter = meterProvider.getMeter(
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  process.env.npm_package_name!,
+  process.env.npm_package_version
+);
+
+const requestCounter = meter.createCounter('request-count', {
+  description: 'Request counter',
+});
+
 const app: Express = express();
 const port = 8080;
 
 const parentSpan = tracer.startSpan('main');
 
 app.get('/', (_: Request, res: Response) => {
+  const attributes = { pid: process.pid, route: 'GET /' };
+  requestCounter.add(1, attributes);
+
   const ctx = opentelemetry.trace.setSpan(
     opentelemetry.context.active(),
     parentSpan
